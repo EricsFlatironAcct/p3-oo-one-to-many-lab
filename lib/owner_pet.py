ea73ebe81458695f9eeae77d77ceab29c6196192
Eric Krukowski <erickruk@amazon.com>
2023-07-09 03:19:14 -0400

    Lab complete

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,30 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all=[]
+    def __init__(self, name, pet_type, owner=None):
+        self.name=name
+        self.set_pet_type(pet_type)
+        self.owner=owner
+        Pet.all.append(self)
+
+    def set_pet_type(self, pet_type):
+        if pet_type in Pet.PET_TYPES: 
+            self.pet_type = pet_type 
+        else:
+            raise Exception
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name=name
+    
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner=self
+        else:
+            raise Exception
+        
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key= lambda n: n.name)
